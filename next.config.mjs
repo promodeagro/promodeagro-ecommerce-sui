289fbf15fd50f3a694c091724286466b2758102d
UmranBatuwah <ubatuwah@gmail.com>
2024-06-20 00:47:47 +0530

    changes in build

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,19 @@
 import bundleAnalyzer from '@next/bundle-analyzer';
 
-const withBundleAnalyzer = bundleAnalyzer({ enabled: process.env.ANALYZE === 'true' });
+const withBundleAnalyzer = bundleAnalyzer({ 
+  enabled: process.env.ANALYZE === 'true' 
+});
 
 const nextConfig = {
+  distDir: 'build', // Custom build output directory
 
   images: {
-  
-    domains: ['ecomdmsservice.s3.amazonaws.com', 'shopify-assets.shopifycdn.com','www.shutterstock.com'],
+    domains: [
+      'ecomdmsservice.s3.amazonaws.com', 
+      'shopify-assets.shopifycdn.com', 
+      'www.shutterstock.com'
+    ],
   },
 };
 
-  
-  export default nextConfig;
-  
+export default withBundleAnalyzer(nextConfig);
